6c4c05da2a350af9ca923f74aa54938ecf92f022
henrikstelter <stelterhenrik@gmail.com>
2021-12-23 20:16:34 +0100

    new site

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,26 +1,16 @@
-let r,g,b;
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
+
+  background('#193723');
   
-  r = 0;
-  g = 0;
-  b = 0;
-  
-  r = map(mouseX, 0, windowWidth, 0, 255);
-  g = map(mouseX, 0, windowWidth, 255, 0);
-  b = map(mouseY, 0, windowHeight, 255, 0);
-  
-  noStroke();
-  fill(r,g,b);
-  
-  ellipse(mouseX,mouseY,windowWidth/4);
-  
-  fill(b,r,g)
-  rectMode(CENTER);
-  rect(windowWidth/2,windowHeight/2,mouseX/2,mouseY/2);
-  
+  noCursor();
+  stroke('#E6DCC8');
+  strokeWeight(25);
+  noFill();
+  //ellipse(mouseX, mouseY, width/50);
+  line(pmouseX, pmouseY, mouseX, mouseY);
+              
 }
