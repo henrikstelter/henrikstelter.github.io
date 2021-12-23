60b2d71326d317f436fcfbdbc5fd78d932bc9c61
henrikstelter <stelterhenrik@gmail.com>
2021-12-23 21:29:27 +0100

    p5 added

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -4,13 +4,21 @@ function setup() {
 
 function draw() {
 
-  background('#193723');
+//  background('#193723');
   
   noCursor();
   stroke('#E6DCC8');
-  strokeWeight(25);
+  strokeWeight(1);
   noFill();
   //ellipse(mouseX, mouseY, width/50);
   line(pmouseX, pmouseY, mouseX, mouseY);
+  
+  strokeWeight(1);
+  rectMode(CENTER);
+  rect(windowWidth/2, windowHeight/2, mouseX/2, mouseY/4);
               
+}
+
+function mousePressed() {
+  clear();
 }
